bb7debdda77eb425aaebc59b4ce555d5214e0e98
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-05-03 13:01:29 +0300

    Update Lesson3_2.py

--- Lesson3_2.py
+++ Lesson3_2.py
@@ -4,9 +4,13 @@ for i in range(2):
     x = str(random.randint(1, 10)) + '.txt'
     print(x)
     with open(x, 'tr') as a:
-        s = a.readline()
-        while s!= '':
-            summ += int(s.rstrip())
+        try:
             s = a.readline()
-        a.close()
+            while s!= '':
+                summ += int(s.rstrip())
+                s = a.readline()
+        except TypeError:
+            print ('В файле ', x, ' ошибка данных')
+        finally:
+            a.close()
 print(summ)
