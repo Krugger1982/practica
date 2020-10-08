7bb16903238c6cef413fa47448eb357103fc4aad
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-10-08 03:16:01 +0300

    Create less_4.py

---
+++ less_4.py
@@ -0,0 +1,24 @@
+import os.path
+
+def crawl (dir_adress='.'):
+    Dirs = []
+    Files = []
+    result = []
+    for root, dirs, files in os.walk(dir_adress):
+        Dirs += dirs
+        Files += files
+    result.append(Files)
+    result.append(Dirs)
+
+    return result
+
+
+def del_dir(dir_adress='.'):
+    for root, dirs, files in os.walk(dir_adress):
+        try:
+            if len(dirs) == 0:
+                for file in files:
+                    os.remove(dir_adress + '/' + file)
+            os.rmdir(dir_adress)
+        except OSError:
+            pass
